ef6fc02aafe5f61579c4ed56c144df718a6ccc6a
Gagandeep Singh <noddynotask@gmail.com>
2026-07-25 21:51:45 +0530

    Improve comments and formatting in AverageofNnum.java
    Refactor code to improve readability and comments.

--- Input Output/AverageofNnum.java
+++ Input Output/AverageofNnum.java
@@ -5,19 +5,19 @@ public class AverageofNnum {
         Scanner i=new Scanner(System.in);
         int sum=0;
         int count =0;
-
+// Through while loop we going to take number until the user press "0".
         while (true) {
             System.out.print("Enter Number : ");
             int n = i.nextInt();
 
             if (n==0) {
                 break;
-
             }
             count++;
-
             sum=sum+n;
         }
+
+    // we dividing total sum of nums to the nums counts to get an average of a num.    
         int avg = sum/count;
         System.out.println("The avg of all number is : "+avg);
     }
