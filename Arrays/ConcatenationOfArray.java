897b55345c8f116eaad4384a6342b19871f107a7
Gagandeep Singh <noddynotask@gmail.com>
2026-08-01 23:34:40 +0530

    Enhance comments in ConcatenationOfArray.java
    Updated code comments for clarity and improved readability.

--- Arrays/ConcatenationOfArray.java
+++ Arrays/ConcatenationOfArray.java
@@ -1,18 +1,25 @@
 public class ConcatenationOfArray {
     public static void main(String[] args) {
-        int[] nums={1,2,3,4};
-        int n=nums.length;
-        int[] ans=new int[2*n];
+        // Input array
+        int[] nums = {1, 2, 3, 4};
+        int n = nums.length;
+        
+        // Create an output array of double length (2 * n)
+        // For an input of size 4, ans will have a size of 8
+        int[] ans = new int[2 * n];
 
+        // Fill both halves of the target array in a single loop
         for (int i = 0; i < nums.length; i++) {
-            ans[i]=nums[i];
-            ans[i+n]=nums[i];
-
-
+            // Place the element in the first half (indices 0 to n-1)
+            ans[i] = nums[i];
+            
+            // Place the same element in the second half (indices n to 2n-1)
+            ans[i + n] = nums[i];
         }
-        for (int i = 0; i < ans.length; i++) {
-            System.out.print(ans[i]+" ");
 
+        // Print the concatenated array to the console
+        for (int i = 0; i < ans.length; i++) {
+            System.out.print(ans[i] + " ");
         }
     }
 }
