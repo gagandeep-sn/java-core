574797275f91e308920ea85f0bf3bfebf3e9f108
Gagandeep Singh <noddynotask@gmail.com>
2026-08-17 23:39:45 +0530

    Improve readability of TwoSum implementation
    Refactor code for clarity and organization.

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -2,25 +2,39 @@ import java.util.Arrays;
 
 public class TwoSum {
     public static void main(String[] args) {
-        int[] nums = {2,7,11,15};
-        int target =9;
+
+        // Input array
+        int[] nums = {2, 7, 11, 15};
+
+        // Target sum
+        int target = 9;
+
+        // Array to store the indices of the two numbers
         int[] ans = new int[2];
 
+        // Select the first number
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
 
-                int sum =nums[i]+nums[j];
-                 if (sum==target) {
-                     ans[0]=i;
-                     ans[1]=j;
-                     System.out.println(Arrays.toString(ans));
-                     return;
+            // Select the second number after the first number
+            for (int j = i + 1; j < nums.length; j++) {
 
-                 }
+                // Calculate the sum of both numbers
+                int sum = nums[i] + nums[j];
 
+                // Check if the sum is equal to the target
+                if (sum == target) {
+
+                    // Store the indices of the two numbers
+                    ans[0] = i;
+                    ans[1] = j;
+
+                    // Print the result
+                    System.out.println(Arrays.toString(ans));
+
+                    // Stop the program after finding the answer
+                    return;
+                }
             }
-
         }
-
     }
 }
