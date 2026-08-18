aca4b99bbc9032f67fc894f73666693ef582814b
Gagandeep Singh <noddynotask@gmail.com>
2026-08-18 23:49:00 +0530

    Refactor Perfectnum to improve factor calculation

--- Math/Perfectnum.java
+++ Math/Perfectnum.java
@@ -2,26 +2,29 @@ import java.util.Scanner;
 
 public class Perfectnum {
     public static void main(String[] args) {
+
+        // Take input from user
         System.out.print("Enter the num : ");
         Scanner i = new Scanner(System.in);
         int num = i.nextInt();
-         int fac=0;
-         int sum=0;
+
+        // Store sum of factors
+        int sum = 0;
+
+        // Find all proper factors of num
         for (int j = 1; j < num; j++) {
 
-            if(num%j==0) {
-                fac=j;
-                sum +=fac;
-
+            // If j divides num completely, it is a factor
+            if (num % j == 0) {
+                sum += j;
             }
-
         }
-        if(num==sum) {
-            System.out.println(num+" is a perfect number.");
 
+        // Check whether sum of factors equals the number
+        if (num == sum) {
+            System.out.println(num + " is a perfect number.");
         } else {
-            System.out.println(num+" is not a perfect number.");
+            System.out.println(num + " is not a perfect number.");
         }
     }
-
 }
