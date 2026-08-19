81d79c8493cd60fc2bf810540c6a1a54b5d6521b
Gagandeep Singh <noddynotask@gmail.com>
2026-08-19 23:18:25 +0530

    adding comment

--- Strings/ReverseString.java
+++ Strings/ReverseString.java
@@ -2,22 +2,33 @@ import java.util.Scanner;
 
 public class ReverseString {
     public static void main(String[] args) {
-        System.out.print("Enter String : ");
+
+        // Create Scanner object to take input from user
         Scanner s = new Scanner(System.in);
-           String str =s.next();
-           int i = str.length()-1;
-           String f = "";
 
-        for (int j = i; j >=0 ; j--) {
+        // Ask the user to enter a string
+        System.out.print("Enter String : ");
 
-            char c=str.charAt(j);
-            f=f+c;
+        // Read the string entered by the user
+        String str = s.next();
 
+        // Start from the last index of the string
+        int i = str.length() - 1;
+
+        // Variable to store the reversed string
+        String f = "";
+
+        // Loop from the last character to the first character
+        for (int j = i; j >= 0; j--) {
+
+            // Get the character at index j
+            char c = str.charAt(j);
+
+            // Add the character to the reversed string
+            f = f + c;
         }
 
+        // Print the reversed string
         System.out.println(f);
-
-
-        }
     }
-
+}
